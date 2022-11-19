5554ff5f04375a6ccf7d7fb84aa10207cedf20ea
Aloysius Gilang <gilangpramdya84@gmail.com>
2022-11-19 18:45:21 +0700

    feat : add navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import Login from './components/loginregister/Login'
-import Register from './components/loginregister/Register'
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from "./components/loginregister/Login";
+import Register from "./components/loginregister/Register";
+import Homepage from "./pages/Homepage";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -9,9 +10,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Homepage />} />
       </Routes>
-    </Router>  
-
+    </Router>
   );
 }
 
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+export default function Login() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+      <Navbar />
+      <h1>Homepage</h1>
+    </div>
+  );
+}
