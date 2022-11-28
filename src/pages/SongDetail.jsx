16daf10a1f9ef2ad1f26fab16824d45f4c3cf769
Aloysius Gilang <gilangpramdya84@gmail.com>
2022-11-28 23:42:05 +0700

    feat : add create and update song

--- src/pages/SongDetail.jsx
+++ src/pages/SongDetail.jsx
@@ -6,7 +6,7 @@ import likedLogo from "../assets/likedLogo.png";
 
 const SongDetail = () => {
   const { songId } = useParams();
-
+  const API_PATH = "http://localhost:8080/src/php/song/editPremiumSong.php";
   const [songDetail, setSongDetail] = useState();
   const [songTitle, setSongTitle] = useState();
 
@@ -17,14 +17,57 @@ const SongDetail = () => {
         .get(`http://localhost:3001/user/10/songs/${songId}`)
         .then((response) => {
           setSongDetail(response.data);
-          console.log("response data :", response.data);
         })
         .catch((error) => {
           console.log(error);
         });
     }
     getSongDetail();
-  }, []);
+  }, [songId]);
+
+  const handleSongTitleChange = (e) => {
+    setSongTitle(e.target.value);
+    console.log("songTitle", songTitle);
+  };
+
+  const handleSongTitleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Clicked", songTitle);
+    try {
+      axios.put(
+        `http://localhost:3001/user/10/songs/title/${songId}`,
+        {
+          title: songTitle,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // handle audio file change to localhost:8080 php
+  const handleAudioFileChange = (e) => {
+    const file = document.getElementById("audioFile").files[0];
+    const formData = new FormData();
+    formData.append("audiofile", file);
+    formData.append("song_id", songId);
+    formData.append("title", songTitle[0].Judul);
+    console.log("formData", formData);
+    try {
+      axios.post(API_PATH, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="h-screen">
@@ -46,21 +89,32 @@ const SongDetail = () => {
           </div>
         </div>
         <div className="h-3/5 pt-10 pl-8">
-          <form action="POST">
-            <div className="flex flex-col ">
+          <div className="flex flex-col ">
+            <form action="" onSubmit={(e) => handleSongTitleSubmit(e)}>
               <label
                 for="JudulLagu"
                 className="text-gray-100 text-sm font-bold tracking-normal mb-2"
               >
-                Email
+                Judul Lagu
               </label>
               <input
                 id="JudulLagu"
                 className=" bg-white font-normal w-64 h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow"
-                value={songDetail && songDetail[0].Judul}
+                value={songTitle}
+                onChange={(e) => handleSongTitleChange(e)}
               />
-            </div>
-            <div className="flex flex-col pt-4 ">
+              <button type="submit" className="text-white text-xl">
+                Save
+              </button>
+            </form>
+          </div>
+
+          <div className="flex flex-col pt-4 ">
+            <form
+              action=""
+              onSubmit={(e) => handleAudioFileChange(e)}
+              encType="multipart/form-data"
+            >
               <label
                 for="audioFile"
                 className="text-gray-100 text-sm font-bold tracking-normal mb-2"
@@ -68,9 +122,11 @@ const SongDetail = () => {
                 Audio File
               </label>
               <input id="audioFile" type="file" />
-            </div>
-            <button type="submit">Submit</button>
-          </form>
+              <button className="text-white text-xl" type="submit">
+                Upload
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
